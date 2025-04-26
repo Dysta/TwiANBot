2f05794a27bd230f6d0e34f44321268122af890f
Dysta <xav4098@gmail.com>
2025-04-26 10:32:12 +0000

    feat: post tweet

--- src/__main__.py
+++ src/__main__.py
@@ -4,15 +4,15 @@ import asyncio
 
 from loguru import logger
 
-from .components import client, task
-from .tasks import scrutins, tweets
-
-bot = client.instance()
+from .components import client
+from .tasks import scrutins
 
 
 async def main():
+    bot = client.instance()
     scrutins.get_scrutins_task.start(bot)
     scrutins.post_scrutins_task.start(bot)
+    # scrutins.upload_scrutin_media.start(bot)
 
     # ? run the bot forever
     event = asyncio.Event()
@@ -22,7 +22,7 @@ async def main():
 if __name__ == "__main__":
     from dotenv import load_dotenv
 
-    load_dotenv()
+    load_dotenv(".env")
     try:
         asyncio.run(main())
     except KeyboardInterrupt:
